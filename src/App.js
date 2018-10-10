1eb6dfa0264e36fc10d18abbca58041673c06a71
Thomas <thomas@Thomass-MacBook-Air.local>
2018-10-10 13:07:14 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import { Route, Link, NavLink } from 'react-router-dom';
+import routes from './routes.js';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <nav className='nav'>
+
           <div>WestSide University</div> 
           <div className='link-wrap'>
-              <div className='links'>Home</div>
-              <div className='links'>About</div> 
+              <Link to="/" className="links">Home</Link>
+              <Link to="/about" className="links">About</Link> 
           </div>
         </nav>
+
+        { routes }
       </div>
     )
   }
